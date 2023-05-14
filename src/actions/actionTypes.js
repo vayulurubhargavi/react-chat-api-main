a05fd45a39291742f1ac419bc06e9a7ce7a84c51
vayulurubhargavi <vayuluru16@gmail.com>
2023-05-14 09:37:01 +0530

    Updates

--- src/actions/actionTypes.js
+++
@@ -1,2 +0,0 @@
-export const FETCH_CONTACTS = 'FETCH_CONTACTS';
-export const ADD_MESSAGE = 'ADD_MESSAGE';
--- src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux';
-import contacts from './contact';
-
-import user from './user';
-export default combineReducers({ contacts, user });
